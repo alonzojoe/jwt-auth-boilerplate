77a730f11c8c3d3277f97bd18b30a933bc0d48ff
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-02-25 15:19:17 +0800

    refactor: bring back middleware api in _construct method of AuthController

--- routes/api.php
+++ routes/api.php
@@ -9,12 +9,11 @@ Route::get('/test', function () {
     return response()->json(['status' => 'success', 'message' => 'API Endpoint Works!'], 200);
 });
 
+
+
 Route::group(['prefix' => '/auth'], function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
-});
-
-Route::group(['prefix' => '/auth', 'middleware' => 'auth:api'], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/me', [AuthController::class, 'me']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
